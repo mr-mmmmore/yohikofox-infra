5269385e9e8088ce0849e8dd789fb2507bcdb459
Gilles Lévy <gilles.l.1@free.fr>
2023-04-15 19:28:16 +0200

    Solve part I using yaml files

---
+++ k8s/part1/B.part1-yaml.sh
@@ -0,0 +1,15 @@
+# I.2.
+# Create an nginx pod
+kubectl apply -f part1.2.yaml
+
+# I.3.
+# Create a deployment with nginx
+kubectl apply -f part1.3.yaml
+# Check the deployment
+kubectl describe deployment nginx-deploy
+
+# 4.
+# Create a deployment of 2 nginx pods and a clusterIP service that target the pods
+kubectl apply -f part1.4.yaml
+# Get the service url and open it in the browser
+minikube service nginx-svc
