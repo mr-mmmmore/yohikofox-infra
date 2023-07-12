d196703f21a92d8cbd87e3c84ca9eb2ae083359e
Gilles Lévy <gilles.l.1@free.fr>
2023-07-12 10:46:15 +0200

    Check that the charts are published and available

--- helm/nginx-service/cmds-part-3.sh
+++ helm/nginx-service/cmds-part-3.sh
@@ -8,3 +8,12 @@ cd .. && helm package nginx-service
 helm repo index --url https://github.com/mr-mmmmore/yohikofox-infra/tree/main/helm .
 
 
+# Check the published repo
+
+# Add the repo
+helm repo add helm-exercise https://mr-mmmmore.github.io/yohikofox-infra/helm
+
+# Search the charts
+helm search repo nginx-service
+# NAME                       	CHART VERSION	APP VERSION	DESCRIPTION                
+# helm-exercise/nginx-service	0.1.0        	1.16.0     	A Helm chart for Kubernetes
